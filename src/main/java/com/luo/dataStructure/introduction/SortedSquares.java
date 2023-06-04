a49a967085c89ceed3ba1a23802d47500ff31b75
lkx <lkx3091705416@126.com>
2023-06-04 20:17:08 +0800

    代码的调整

--- src/main/java/com/luo/dataStructure/introduction/SortedSquares.java
+++ src/main/java/com/luo/dataStructure/introduction/SortedSquares.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
  */
 public class SortedSquares {
     public int[] sortedSquares(int[] nums) {
-        int m = nums.length - 1, result = 0, i, j = 0;
+        int m = nums.length - 1, result, i, j = 0;
         int[] res = new int[m + 1];
         for (i = 0; i <= m; i++) {
             int temp = nums[i];
